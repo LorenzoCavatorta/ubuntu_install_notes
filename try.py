dd397ac065eb7310b78bee3a9b6c9ac2fbe932e2
ll <lorenzo.cavatorta.pr@gmail.com>
2016-09-14 23:48:10 +0100

    start working on configs

--- try.py
+++ try.py
@@ -1,6 +1,6 @@
-from tryconfig import all_programs
+from tryconfig import all_programs, chosen_programs
 from tryconfig import defaults
-from tryconfig import chosen_programs
+from tryconfig import all_configs, config_step_types, chosen_configs
 from subprocess import STDOUT, check_call
 from types import *
 
@@ -15,8 +15,10 @@ class bashcommand:
     def __add__(self, other):
         if type(other) is StringType:
             self.command += ' ' + other
+            return
         if type(other) is ListType:
             self.command += ' ' + ' '.join(l)
+            return
 
     def run(self, choseprintlog=False):
         self.stdlog = os.popen(self.command)
@@ -32,11 +34,11 @@ class program:
 
     def __init__(self, s, program_dict):
         self.name = s
-        self.config = program_dict
+        self.setup_instructions = program_dict
 
     def get_attr(self,attr):
-        if attr in self.config:
-            return self.config[attr]
+        if attr in self.setup_instructions:
+            return self.setup_instructions[attr]
         else:
             try:
                 if attr == 'realname':
@@ -66,9 +68,10 @@ class program:
             return
         else:
             pre = self.get_attr('repo_prequel')
+            #pdb.set_trace()
             for c in pre:
                 command = bashcommand(c)
-                command.run()
+                command.run(True)
             repo_adder = bashcommand(self.get_attr('repo_add_command'))
             repo_adder + repo
             repo_adder.run(True)
@@ -79,9 +82,50 @@ class program:
     def install(self):
         self.add_repo()
         self.run_install()
+        self.prog_config()
 
 
+    def prog_config(self):
+        c = self.get_attr('config')
+        if c == defaults['config']:
+            return
+ #       else:
+#        for i in config_steps:
+            #move config
+            #add config lines
+            #
+
+class configuration:
+
+    def __init__(self,name,config_instrucions):
+        self.name = name
+        self.steps = config_instrucions
+
+    def check_steps(self):
+        for s in self.steps:
+            assert s['type'] in config_step_types, 'error, command type not allowed'
+            if s['type'] not in config_step_types:
+                   return 'error, command type not allowed'
+        return
+
+    def run_step(self,step):
+        if step['type'] == 'bash_command':
+            command = bashcommand(step['content'])
+            command.run(True)
+
+    def run_all_steps(self):
+        self.check_steps()
+        for s in self.steps:
+            self.run_step(s)
+
+    
+        
 for p in chosen_programs:
     prog = program( p , all_programs[p])
     print(prog.build_install_command())
     prog.install()
+
+for c in chosen_configs:
+    conf = configuration( c, all_configs[c] )
+    conf.run_all_steps()
+    
--- tryconfig.py
+++ tryconfig.py
@@ -1,19 +1,38 @@
 #programs = {'em': {'realname':'emacs'} }
 
 #chosen_programs = ['emacs', 'audacity', 'mp3_dec', 'pycharm']
-chosen_programs = ['sublime']
-#chosen_programs = ['emacs']
+chosen_programs = ['emacs']
+#chosen_programs = ['sublime']
+#chosen_programs = ['google_music']
+
+chosen_configs = [ 'caps2ctrl' ]
 
 all_programs = {
-    'emacs'     : {},
-    'audacity'  : {},
-    'mp3_dec'   : {'realname' : 'ffmpeg'},
-    'beets'     : {},
-    'vlc'       : {},
-    'gimp'      : {},
-    'pycharm'   : {'realname' : 'pycharm-community', 'repo' : 'ppa:mystic-mirage/pycharm'},
-    'sublime'   : {'realname' : 'sublime-text-installer', 'repo' : 'ppa:webupd8team/sublime-text-3'}
-            }
+    'emacs'         : {},
+    'audacity'      : {},
+    'mp3_dec'       : {'realname' : 'ffmpeg'},
+    'beets'         : {},
+    'vlc'           : {},
+    'gimp'          : {},
+    'pycharm'       : {'realname' : 'pycharm-community', 'repo' : 'ppa:mystic-mirage/pycharm'},
+    'sublime'       : {'realname' : 'sublime-text-installer', 'repo' : 'ppa:webupd8team/sublime-text-3'},
+    #'google_music'  : {'realname' : '', 'repo_prequel' : ['wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - '], 'repo' : 'sudo sh -c \'echo "deb [arch=amd64] http://dl.google.com/linux/musicmanager/deb/ stable main" >> /etc/apt/sources.list.d/google.list\'' , 'repo_add_command' : ''}
+    'google_music'  : {'realname' : 'google-musicmanager-beta',
+                       'repo_prequel' : ['wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - '],
+                       'repo' : '\'echo "deb [arch=amd64] http://dl.google.com/linux/musicmanager/deb/ stable main" >> /etc/apt/sources.list.d/google.list\'' ,
+                       'repo_add_command' : 'sudo sh -c'},
+    'pip'           : {'realname' : 'python-pip'},
+}
+
+
+#every config is a list of steps to do,
+#every step is a dic with a standard structure, type of action, actual action
+config_step_types = {'bash_command':'single command to run in bash'}
+
+all_configs = {
+    'caps2ctrl' : [ {'type':'bash_command', 'content':'setxkbmap -option ctrl:nocaps'} ],
+}
+
 
 defaults = {'install_command' : 'apt-get -y install',
             'is_sudo' : 'sudo',
@@ -21,5 +40,8 @@ defaults = {'install_command' : 'apt-get -y install',
             'repo' : 'STNDRD',
             'repo_add_command' : 'sudo add-apt-repository -y',
             'repo_prequel' : '',
-            'repo_update_command' : 'sudo apt-get update'
+            'repo_update_command' : 'sudo apt-get update',
+            'config' : 'no'
             }
+
+
